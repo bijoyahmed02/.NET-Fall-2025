8769ef2bb1f39888929c0f0be683343c67277072
bijoyahmed02 <bijoyahmed818@gmail.com>
2026-04-22 00:03:23 +0600

    Interface Segregation

---
+++ Intro3Tier/DAL/DataAccessFactory.cs
@@ -0,0 +1,26 @@
+﻿using DAL.EF;
+using DAL.EF.Models;
+using DAL.Interfaces;
+using DAL.Repos;
+using System.Reflection.Emit;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        UMSContext db;
+        public DataAccessFactory(UMSContext db) { 
+            this.db = db;
+        }
+        public IRepository<Department> DepartmentData() { 
+            return new DepartmentRepo(db);
+        }
+        public IDepartmentFeature DepartmentFeature() {
+            return new DepartmentRepo(db);
+        }
+        public IRepository<Student> StudentData() {
+            return new StudentRepo(db);
+        }
+
+    }
+}
---
+++ Intro3Tier/DAL/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using DAL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepository<T>
+    {
+        bool Create(T s);
+        T Get(int id);
+        List<T> Get();
+        bool Update(T s);
+        bool Delete(int id);
+   
+    }
+}
